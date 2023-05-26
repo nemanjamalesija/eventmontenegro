626d5081b250bda6f01fc9b2a23e980213ef1195
nemanjamalesija <malesija13@gmail.com>
2023-05-26 20:39:14 +0200

    Hero nav full responsive

--- src/app/Home/Hero/Hero.tsx
+++ src/app/Home/Hero/Hero.tsx
@@ -8,21 +8,21 @@ import hero from '../../../../assets/img/hero.png';
 const Hero: FC = () => {
   return (
     <section className='pt-4 sm:pt-12 px-5'>
-      <div className='flex flex-col sm:grid sm:grid-cols-custom sm:items-start gap-20 sm:gap-24 mx-auto max-w-custom'>
+      <div className='flex flex-col lg:grid lg:grid-cols-custom lg:items-start gap-20 lg:gap-24 mx-auto max-w-custom'>
         <div className='mt-4 sm:mt-20'>
           <h1
             className={clsx(
               josefinSans.className,
-              'heading-primary text-center sm:text-start text-4xl sm:text-5xl font-bold text-gray-600'
+              'heading-primary text-center lg:text-start text-4xl md:text-5xl font-bold text-gray-600'
             )}
           >
             Stvaramo nezaboravna sjećanja koja traju vječno
           </h1>
-          <p className='text-lg sm:text-16rem leading-7 sm:leading-9 text-center sm:text-start  text-gray-700 font-normal  mt-4 sm:mt-6'>
+          <p className='text-lg md:text-16rem leading-7 lg:leading-9 text-center lg:text-start  text-gray-700 font-normal  mt-4 sm:mt-6'>
             Iznajmljivanje opreme, organizacija i koordinacija proslava.
             Prilagođeno vašim ličnim potrebama i detaljnim željama.
           </p>
-          <div className='mt-10 flex justify-center sm:justify-start gap-6'>
+          <div className='mt-10 flex justify-center lg:justify-start gap-6'>
             <Button className='px-4 py-2 sm:py-3 sm:px-6 sm:text-xl'>
               Počni svoj projekat
             </Button>
--- src/app/Layout/Navigation/Navigation.tsx
+++ src/app/Layout/Navigation/Navigation.tsx
@@ -13,12 +13,12 @@ const Navigation: FC = () => {
   const { isNavVisible, setIsNavVissible } = useAppContext();
 
   useEffect(() => {
-    if (window.innerWidth >= 600 && !isNavVisible) setIsNavVissible(true);
+    if (window.innerWidth >= 1000 && !isNavVisible) setIsNavVissible(true);
 
     const handleResize = () => {
-      if (window.innerWidth >= 600 && !isNavVisible) setIsNavVissible(true);
+      if (window.innerWidth >= 1000 && !isNavVisible) setIsNavVissible(true);
 
-      if (window.innerWidth <= 600 && isNavVisible) {
+      if (window.innerWidth <= 1000 && isNavVisible) {
         setIsNavVissible(false);
       }
     };
@@ -31,8 +31,8 @@ const Navigation: FC = () => {
   }, [isNavVisible, setIsNavVissible]);
 
   return (
-    <header className='nav__header col-start-1 row-start-1 col-span-2 flex justify-between items-center h-20 sm:pr-20 sm:pl-20 px-6 z-50'>
-      <div className='flex gap-2 items-center z-200'>
+    <header className='nav__header flex justify-between items-center h-20 sm:px-20 px-6 z-50'>
+      <div className='flex gap-2 items-center'>
         <Image
           src={logo}
           alt='Picture of the author'
@@ -47,14 +47,14 @@ const Navigation: FC = () => {
       </div>
       {!isNavVisible ? (
         <button
-          className='sm:hidden absolute top-5 right-10 z-50 block'
+          className='lg:hidden absolute top-5 right-10 z-50 block'
           onClick={() => setIsNavVissible(!isNavVisible)}
         >
           <AlignJustify className='h-9 w-9 stroke-slate-700' />
         </button>
       ) : (
         <button
-          className='sm:hidden absolute top-5 right-10 z-50 block '
+          className='lg:hidden absolute top-5 right-10 z-50 block '
           onClick={() => setIsNavVissible(!isNavVisible)}
         >
           <X className='h-9 w-9 stroke-slate-700' />
@@ -62,16 +62,15 @@ const Navigation: FC = () => {
       )}
       <nav
         className={cn(
-          'bg-white sm:bg-bg-general bg-opacity-80 sm:bg-opacity-1 backdrop-blur-md sm:backdropbackdrop-blur-0 flex items-center justify-center absolute top-0 left-0 w-full h-full sm:static sm:w-auto sm:h-auto z-40 transition-all duration-500 ease-in sm:translate-x-0',
+          'bg-white lg:bg-bg-general bg-opacity-80 lg:bg-opacity-1 backdrop-blur-md lg:backdropbackdrop-blur-0 flex items-center justify-center absolute top-0 left-0 w-full h-full lg:static lg:w-auto lg:h-auto z-40 transition-all duration-500 ease-in lg:translate-x-0',
           {
             'opacity-0 translate-x-full pointer-events-none invisible':
               !isNavVisible,
             'opacity-1 translate-x-0 pointer-events-auto visible': isNavVisible,
-          },
-          'sm:translate-x-0'
+          }
         )}
       >
-        <ul className='flex flex-col sm:flex-row  gap-11 sm:gap-16 items-center text-gray-600 font-bold sm:font-medium text-2xl sm:text-xl'>
+        <ul className='flex flex-col lg:flex-row  gap-11 lg:gap-16 items-center text-gray-600 font-bold lg:font-medium text-2xl lg:text-xl'>
           <li className='nav__ul--item'>
             <a href='#'> Početna</a>
           </li>
@@ -90,7 +89,7 @@ const Navigation: FC = () => {
           <li className='nav__ul--item'>
             <a
               href='#'
-              className='active:scale-95 py-2 px-10 bg-color-accent-main hover:bg-color-shade-main transition-all, duration-300 rounded-sm text-color-shadest inline-flex items-center justify-center text-2xl sm:text-lg font-bold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2'
+              className='active:scale-95 py-2 px-6 bg-color-accent-main hover:bg-color-shade-main transition-all, duration-300 rounded-sm text-color-shadest inline-flex items-center justify-center text-2xl lg:text-lg font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2'
             >
               Kontakt
             </a>
