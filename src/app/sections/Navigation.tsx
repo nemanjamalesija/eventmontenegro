53da082d5840a32cdf1d6e54f2c5d4c13167e214
nemanjamalesija <malesija13@gmail.com>
2023-05-28 16:41:35 +0200

    Fix nav bug

--- src/app/sections/Navigation.tsx
+++ src/app/sections/Navigation.tsx
@@ -65,7 +65,7 @@ const Navigation: FC = () => {
       )}
       <nav
         className={cn(
-          'nav absolute top-0 left-0 w-full h-screen lg:static lg:w-auto lg:h-full bg-white lg:bg-bg-general lg:backdropbackdrop-blur-0 flex items-start lg:items-center justify-center  z-40 transition-translate duration-500 opacity-1 ease-in backdrop-blur-md',
+          'nav absolute top-0 left-0 w-full h-custom lg:static lg:w-auto lg:h-full bg-white lg:bg-bg-general lg:backdropbackdrop-blur-0 flex items-start lg:items-center justify-center  z-40 transition-translate duration-500 opacity-1 ease-in backdrop-blur-md',
           {
             'opacity-0 translate-x-full pointer-events-none invisible':
               !isNavVisible,
