4b302f6895395c39572cd2a30822e96347de270a
nemanjamalesija <malesija13@gmail.com>
2023-05-29 13:41:06 +0200

    Add reviews

--- src/app/sections/Reviews.tsx
+++ src/app/sections/Reviews.tsx
@@ -5,9 +5,70 @@ import usernebuska from '../../../assets/img/usernebuska.jpg';
 import usernemanja from '../../../assets/img/usernemanja.jpg';
 import usernikola from '../../../assets/img/usernikola.jpg';
 import usertanja from '../../../assets/img/usertanja.jpg';
+import Image from 'next/image';
+
+const reviews = [
+  {
+    image: useramar,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: 'Amar Smaković',
+  },
+  {
+    image: userelisa,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: '- Elisa Vincent',
+  },
+  {
+    image: usernebuska,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: '- Nebojša i Ena Bojić',
+  },
+  {
+    image: usernemanja,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: '- Nemanja Malesija',
+  },
+  {
+    image: usernikola,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: '- Nikola Vukčević',
+  },
+  {
+    image: usertanja,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita dolorem iusto quo maiores explicabo excepturi vero, ipsam quasi ',
+    author: '- Ivana Boljević',
+  },
+];
 
 const Reviews: FC = () => {
-  return <section className='py-24 bg-color-lightest'></section>;
+  return (
+    <section className='py-24 px-10 bg-color-lightest'>
+      <div className='grid grid-cols-custom2fr '>
+        <div className='reviews grid grid-cols-custom2fr gap-x-16 gap-y-8'>
+          {reviews.map((r, i) => {
+            return (
+              <div key={i} className='review'>
+                <div className='review__author mb-4'>
+                  <Image
+                    src={r.image}
+                    alt={r.author}
+                    className='h-16 w-16 rounded-full object-cover'
+                  />
+                </div>
+                <p className='review__text mb-6 text-lg text-gray-700'>
+                  {r.text}
+                </p>
+                <span className='review__author text-base text-gray-600'>
+                  {r.author}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+        <div className='images'></div>
+      </div>
+    </section>
+  );
 };
 
 export default Reviews;
