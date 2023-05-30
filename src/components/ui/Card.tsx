94c13011dc2c0a569306ca74101c8455ccd22f8f
nemanjamalesija <malesija13@gmail.com>
2023-05-30 23:30:44 +0200

    Advance card design

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -17,12 +17,13 @@ const Card = ({ cardNumber, cardHeadingText }: CardProps) => {
           {cardHeadingText}
         </span>
       </h4>
-      <div className='card__cta'>
+      <div className='card__cta'></div>
+      <div className='card__buttons flex items-center justify-center'>
         <Link
-          className='card__link active:scale-95 transition-all, duration-300 rounded-sm inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 sm:py-3 sm:px-6 sm:text-xl bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
+          className='active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 sm:py-3 sm:px-6 sm:text-xl bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
           href='/vjencanja'
         >
-          Saznaj više
+          Saznajte više
         </Link>
       </div>
     </div>
