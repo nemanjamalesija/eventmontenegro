39936010a1f18f70e10896aeb7b704d6989117b4
nemanjamalesija <malesija13@gmail.com>
2023-05-30 23:48:29 +0200

    Finish card design

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logoSub from '../../../assets/img/logo-sub.png';
 
 type CardProps = {
   cardNumber: number;
@@ -17,10 +19,16 @@ const Card = ({ cardNumber, cardHeadingText }: CardProps) => {
           {cardHeadingText}
         </span>
       </h4>
-      <div className='card__cta'></div>
+      <div className='card__cta relative'>
+        <Image
+          src={logoSub}
+          className='card__image h-full w-auto'
+          alt='logo event montenegro'
+        />
+      </div>
       <div className='card__buttons flex items-center justify-center'>
         <Link
-          className='active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 sm:py-3 sm:px-6 sm:text-xl bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
+          className='card__link active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 sm:py-3 sm:px-6 sm:text-xl bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
           href='/vjencanja'
         >
           Saznajte više
