d4338d56b0cdb9f42b796695dcc91ba187f37f07
nemanjamalesija <malesija13@gmail.com>
2023-06-03 12:31:23 +0200

    Add kids categories

--- src/app/KidsEvents/layout.tsx
+++ src/app/KidsEvents/layout.tsx
@@ -7,6 +7,91 @@ type LayoutProps = {
   children: ReactNode;
 };
 
+const kidsOffers = [
+  {
+    category: 'Maskote',
+    content:
+      'Možete li zamisliti izraz na licu djeteta kada se njegov omiljeni junak iz crtanog filma pojavi na zabavi? Naši animatori se oblače u kostime Mickey Mouse, Minnie Mouse, Nodi, Pepa prase, Sunđer Bob, Hello Kitty, Frozen, Winnie Pooh, Paw patrol i druge, sa ciljem da slavlje Vašem miljeniku učine posebnim. Maskota po Vašem izboru će iznenaditi djecu svojim dolaskom, plesati i igrati, što za najmlađi uzrast predstavlja pravo oduševljenje. ',
+  },
+  {
+    category: 'Klovn',
+    content:
+      'Zabava sa klovnom donosi puno radosti i smjeha. Program klovna obuhvata razne igre i plesne performanse, kao i modeliranje balona i balone od sapunice.',
+  },
+  {
+    category: 'Mađioničar',
+    content:
+      'Želite li da za rođendan svom mališanu priuštite nešto posebno, što će ludo zabaviti njega i njegove drugare? Pravi izbor je mađioničar sa svojim sjajnim trikovima koje vrijedi vidjeti. Program mađioničara je u vidu predstave i traje do 40 minuta što je sasvim dovoljno da se djeci održi pažnja. Program je interaktivnog karaktera. Za rodjendansku proslavu slavljenik dobija poklon iznenađenja kroz mađioničarski trik. Utisak koji program mađioničara ostavlja dugo se pamti i prepričava. ',
+  },
+  {
+    category: 'Princeze',
+    content:
+      'Svaka djevojčica mašta da postane princeza. Kroz ovu zabavu njeni snovi postaju stvarnost. U ponudi imamo kostime princeza Rapunzel, Uspavana ljepotica i sirene Ariel koje oblače naše animatorke. Zabava “Kako postati princeza” uključuje princezu po izboru, bajku, pjevanje, ples, bonton i igrice prilagođene uzrastu djece.',
+  },
+  {
+    category: 'Super heroji',
+    content:
+      'Naši animatori oblače kostime Spidermana, Supermana ili Betmena. Kroz ovu zabavu djeca upostaju pravi super heroji. Zabava uključuje heroja po izboru i tematske igre sa raznim rekvizitima. ',
+  },
+  {
+    category: 'Crtanje po licu',
+    content:
+      'Specijalnim bojama za lice pretvaramo nasmijana dječija lica u leptiriće, ribice, gusare, princeze, mačkice, štrumfove i razne likove iz crtanih filmova i bajki koje djeca obožavaju. Za crtanje po licu koristimo proizvode koji prijaju koži, koji su netoksični i lako se skidaju vodom.',
+  },
+  {
+    category: 'Gusari',
+    content:
+      'Uz gusarsku muziku i efekte, potragu za blagom i savladavanje prepreka, djeca će doživjeti pravu gusarsku avanturu. Slavljenik i njegovi drugari dobijaju mačeve od balona, tako da je borba gusara neizostavni dio ove zabave. ',
+  },
+  {
+    category: 'Modeliranje balona',
+    content:
+      'Modeling baloni su omiljeni kod mališana. Možemo da napravimo specijalnu kreaciju od balona za slavljenika, ali i razne modeling figure za njegove drugare, razne životinje, cvjetiće, kape, krune, mačeve i slično, tako da svaki mali gost na rođendanu dobije poklon kojim će biti oduševljen.',
+  },
+  {
+    category: 'Pinjata',
+    content:
+      'Pinjata je tradicionalna meksička igra koja se igra prilikom raznih proslava, a naročito na dječijim rođendanima. Pinjate radimo ručno i u oblicima koji sami odaberete. Najradosniji trenutak nastaje kada se razbije pinjata, jer se tada dolazi do njenog sadržaja. Možemo ih puniti balonima, slatkišima i sitnim igračkama. ',
+  },
+  {
+    category: 'Bioskop na otvorenom',
+    content:
+      'Želite li zabavu pod otvorenim nebom?  Ovo je idealno rešenje za zabavu na plaži ili u dvorištu. Pored mogućnosti gledanja animiranih filmova, djeca mogu uživati u plesu i pjesmi, gledajući omiljene spotove na velikom Disney ekranu. ',
+  },
+  {
+    category: 'DJ & Disko',
+    content:
+      'Disko zabava je idealna za djecu koja se osjećaju “odraslom”. Pravimo diskoteku u bilo kojem zatvorenom prostoru. Pratimo trendove u pogledu muzičkih hitova. Na ovoj zabavi djeca uživaju u plesu, pjesmi i specijalnim efektima. ',
+  },
+  {
+    category: 'Maskenbal',
+    content:
+      'Djeca jako vole da se maskiraju. Oni se rado pretvaraju u svoje omiljene likove iz crtanog filma, bajke ili mašte. Tako maskirani učestvuju u zabavi i plesu, a na kraju defiluju kako bi odraslima prezentovali likove koje predstavljaju. Defile je uvijek praćen odgovarajućom muzikom, kao i aplauzom od strane roditelja i prisutnih. Radimo na pripremi, organizaciji i koordinaciji kompletnog programa.  ',
+  },
+  {
+    category: 'Karaoke',
+    content:
+      'Ovo je idealna zabava za djecu koja vole da pjevaju. Potrebno je samo da iz naše široke ponude pjesama, domaćih ili stranih, odaberu omiljene i čitajući tekst sa ekrana učine zabavu na kojoj će se svi veseliti i pjevati. Voditelj na kraju programa proglašava pobjednika.',
+  },
+  {
+    category: 'Grupna animacija',
+    content:
+      'Animacija je prilagođena za organizovane grupe djece iz inostranstva koja odmor provode u Crnoj Gori i borave u odmaralištima i hotelima. Animacija predviđa grupe od 50 do 200 učesnika, kao i rad sa manjim grupama. Naš program prilagođavamo različitom uzrastu i može da obuhvata jutarnju, dnevnu i večernju animaciju. U saradnji sa svojim partnerima, možemo za njih da organizujemo zabavno-edukativne izlete. ',
+  },
+  {
+    category: 'Novogodišnju program',
+    content:
+      'Želite li da Deda Mraz donese paketić Vašem miljeniku na kućnu adresu?   Bogata novogodišnja ponuda uključuje profesionalne kostime Deda Mraza, animatore, novogodišnju predstavu, novogodišnju balonsku dekoraciju, djeljenje paketića kao i cjelokupan novogodišnji program. Ukoliko Vam je tokom novogodišnjih praznika potreban samo Deda Mraz, pozovite nas, jer u ponudi imamo više profesionalnih kostima.   ',
+  },
+  {
+    category: 'Ostale zabave',
+    content:
+      'Organizujemo i druge vrste zabava za djecu kao što su ples mažoretki, pjena party, kreativne radionice, igre bez granica. Kontaktirajte nas za više informacija.   ',
+  },
+];
+
+const categories = kidsOffers.map((o) => o.category);
+
 const layout = async ({ children }: LayoutProps) => {
   return (
     <section className='py-24 lg:px-10 bg-color-lightest border-t-2 border-b-2 border-white'>
