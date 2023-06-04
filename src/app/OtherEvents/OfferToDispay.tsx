c5eb18b9c3c02eb4cff35d812c2cf6df5f8f762a
nemanjamalesija <malesija13@gmail.com>
2023-06-04 19:16:53 +0200

    Add other events page

---
+++ src/app/OtherEvents/OfferToDispay.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { FC, useEffect, useState } from 'react';
+import { otherOffers } from './otherOffers';
+import clsx from 'clsx';
+
+const OfferToDispay: FC = () => {
+  const [currentCategory, setCurrentCategory] = useState('Plesovi');
+  const [currentOffer, setCurrentOffer] = useState(otherOffers[0]);
+
+  useEffect(() => {
+    const offerToDisplay = otherOffers.filter(
+      (o) => o.category === currentCategory
+    );
+
+    const { category, content } = offerToDisplay[0];
+
+    setCurrentOffer({ category, content });
+  }, [currentCategory]);
+
+  return (
+    <div className='flex flex-col lg:grid grid-cols-2 gap-x-24 gap-5 mb-12 lg:mb-16'>
+      <div
+        className={clsx(
+          'modal grid grid-cols-2 gap-y-[10px] py-2 px-4  rounded-md justify-between'
+        )}
+      >
+        {otherOffers.map((c, i) => (
+          <span
+            key={i}
+            className={clsx(
+              'text-center text-base lg:text-lg font-semibold cursor-pointer transition-all duration-300 ',
+              {
+                'text-color-lightest': c.category === currentCategory,
+                'text-color-accent-darkest': c.category !== currentCategory,
+              }
+            )}
+            onClick={() => setCurrentCategory(c.category)}
+          >
+            {c.category === currentCategory
+              ? `${c.category} \u2190`
+              : c.category}
+          </span>
+        ))}
+      </div>
+      <div
+        className={clsx(
+          'p-1 text-color-gray-light-1 rounded-md justify-between py-2 px-4'
+        )}
+      >
+        <p className={clsx('text-base lg:text-lg  text-gray-600')}>
+          {currentOffer.content}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default OfferToDispay;
---
+++ src/app/OtherEvents/otherOffers.ts
@@ -0,0 +1,42 @@
+export const otherOffers = [
+  {
+    category: 'Plesovi',
+    content:
+      'Ukoliko želite da goste obradujete zabavno-scenskim programom, predlažemo moderan ples, salsu ili trbušni ples, kao izvrsnu zabavu u kojoj prisutni mogu da se opuste i uživaju.  ',
+  },
+  {
+    category: 'Ples sa vatrom',
+    content:
+      'Ples sa vatrom je rijetka disciplina opasnog egzibicionizma. Iskusni plesači svojom sposobnošću i koncentracijom drže pod kontrolom vatrene baklje, koje sa velikom brzinom i preciznošću, vrte oko tijela. Ova kombinacija igre, gimnastike, pokreta iz borilačkih vještina i muzike ostavlja snažan efekat na publiku.  ',
+  },
+  {
+    category: 'Mađioničar',
+    content:
+      'Sjajan način da zabavite svoje goste je uz pomoć magije. Mađioničar nudi predstavu interaktivnog karaktera uz pregršt interesantnih i nevjerovatnih trikova. Svoje trikove može izvoditi na bini ili prolazeći kroz publiku i individualno im prikazivati neke od svojih trikova.',
+  },
+  {
+    category: 'Barmen show',
+    content:
+      'Ukoliko želite da na Vašu zabavu dođe barmen, Vaši gosti će uživati ne samo u atraktivnoj vještini žongliranja sa bocama, već i u ispijanju najboljih koktela. Uz energičnu muzičku podlogu, barmen će pokazati svoje profesionalne sposobnosti, umjeće i spretnost. ',
+  },
+  {
+    category: 'Muzika uživo',
+    content:
+      'Možete se opredjeliti za instrumental muziku i omogućiti Vašim gostima da uživaju u opuštenoj atmosferi uz zvuke gitare, violine ili saksofona. Ukoliko se opredjelite za harmoniku, muzički bend ili DJ-a, Vaša zabava će sigurno poprimiti veseliji tok, a od svojih gostiju možete očekivati ne samo da zapjevaju već i da zaigraju.',
+  },
+  {
+    category: 'Folklor',
+    content:
+      'Folklor čine razni muzičko-scenski prikazi. Narodna kola i plesove izvode članovi folklora iz raznih krajeva Crne Gore i predstavljaju se dobro osmišljenom koreografijom. Svaki nastup je scenski efektan i originalan.  ',
+  },
+  {
+    category: 'Pjena party',
+    content:
+      'Pjena party je nezaboravna zabava. Pjena je potpuno neškodljiva za ljude i okolinu, prijatnog je mirisa, ne iritira sluzokožu i ne isušuje kožu. Kontaktirajte nas unaprijed zbog usklađivanja termina i rasporeda.',
+  },
+  {
+    category: 'Aqua aerobik & aqua dance',
+    content:
+      'Aerobik u vodi je zdrava aktivnost koja tijelo održava u idealnoj formi. Vježbe u aqua aerobiku su raznovrsne, a prednosti su mali rizik od povrede, rad na cijelom tijelu i vježbanje na otvorenom. Kako bi ovaj oblik zabave bio što zanimljiviji kombinujemo muziku i pokrete u vodi, tj. radimo aqua dance ili ples u vodi. Poznavanje stranih jezika, inovativnost u pogledu koreografija, ljubaznost i srdačnost su odlike naših instruktora, što gosti brzo prepoznaju.',
+  },
+];
