c196533636f90c71e18cb29cba305eb208e20bcb
nemanjamalesija <malesija13@gmail.com>
2023-06-06 21:18:03 +0200

    Restyle cards

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -25,7 +25,7 @@ const Card = ({ cardNumber, cardHeadingText, href }: CardProps) => {
           bg-cover bg-center lg:bg-top card__picture--${cardNumber}`}
       >
         &nbsp;
-        <h4 className='card__heading text-2xl  lg:text-[1.8rem] text-white absolute left-0 bottom-2 lg:bottom-3 text-left uppercase'>
+        <h4 className='card__heading text-xl lg:text-2xl   text-white absolute left-0 bottom-[6px] lg:bottom-2  text-left uppercase'>
           <span
             className={`card__heading-span card__heading-span--${cardNumber} font-light py-2 px-4`}
           >
@@ -34,17 +34,10 @@ const Card = ({ cardNumber, cardHeadingText, href }: CardProps) => {
         </h4>
       </div>
 
-      <div className='card__cta p-6 bg-white bg-center bg-blend-screen bg-no-repeat flex items-center justify-center'>
-        <Image
-          src={logoSub}
-          className='card__image h-16 lg:h-full w-auto inline-block'
-          alt='logo event montenegro'
-        />
-      </div>
-      <div className='card__buttons p-8 flex items-center justify-center'>
+      <div className='card__cta pt-20 pb-8 bg-center bg-blend-screen bg-no-repeat flex items-center justify-center'>
         <Link href={href}>
           <button
-            className='card__link active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-6 py-2 sm:py-3 sm:px-6 text-base sm:text-xl  bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
+            className='card__link active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 text-base sm:text-lg  bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
             onClick={removeNavSticky}
           >
             Saznajte više
