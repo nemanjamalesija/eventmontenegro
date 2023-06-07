f3817128bfe8d0b0ebc8288e4fd79019da901dfc
nemanjamalesija <malesija13@gmail.com>
2023-06-07 16:52:46 +0200

    Restyle cards

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -19,25 +19,21 @@ const Card = ({ cardNumber, cardHeadingText, href }: CardProps) => {
   };
 
   return (
-    <div className='card'>
+    <div className='card '>
+      <h4 className='card__heading text-lg sm:text-xl font-medium tracking-wide uppercase text-color-accent-main mb-3'>
+        {cardHeadingText}
+      </h4>
       <div
-        className={`relative h-60 lg:h-80 bg-blend-screen
+        className={`card-photo h-96 
           bg-cover bg-center lg:bg-top card__picture--${cardNumber}`}
       >
         &nbsp;
-        <h4 className='card__heading text-xl lg:text-2xl   text-white absolute left-0 bottom-[6px] lg:bottom-2  text-left uppercase'>
-          <span
-            className={`card__heading-span card__heading-span--${cardNumber} font-light py-2 px-4`}
-          >
-            {cardHeadingText}
-          </span>
-        </h4>
       </div>
-
-      <div className='card__cta pt-20 pb-8 bg-center bg-blend-screen bg-no-repeat flex items-center justify-center'>
+      <div className='card__cta bg-center  bg-no-repeat flex items-center justify-center '></div>
+      <div className='btn'>
         <Link href={href}>
           <button
-            className='card__link active:scale-95 transition-all, duration-300 rounded-full inline-flex items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-4 py-2 text-base sm:text-lg  bg-white hover:bg-color-gray-light-1 text-gray-600 border-color-accent-main'
+            className='card__link active:scale-95 transition-all, duration-300 block items-center justify-center font-semibold transition-color focus:outline-none focus:ring-8 focus:ring-ring-color-primary focus:ring-offset-2 cursor-pointer px-7 py-4  w-full bg-white hover:bg-color-gray-light-1  border-color-accent-main  text-white text-base sm:text-lg uppercase'
             onClick={removeNavSticky}
           >
             Saznajte više
